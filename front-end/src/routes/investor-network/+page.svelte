8dadc30ea85c4d2a2706ace72384a3987310dd1a
algeoffrey <algaza@tebengan.id>
2024-08-12 23:19:48 +0700

    Add investor network page with header and sub-header

---
+++ front-end/src/routes/investor-network/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Navbar from '../../lib/Navbar.svelte';
+    import PageHeader from '../../lib/PageHeader.svelte'
+  </script>
+  
+  <Navbar theme="black" />
+
+  <PageHeader
+  title="ALT Collective | Investor Network";
+  backgroundColor="#19D4E6"
+  secondaryColor="#FC4DEB"
+  titleColor="#000000"
+  subHeaderTextColor= "#FFFFFF"
+  subHeaderLinks={[
+    { name: "Network Membership", href: "/investor-network#network-membership" },
+    { name: "Track Record", href: "/investor-network#track-record" },
+    { name: "FAQs", href: "/investor-network#faqs" }
+  ]}
+/>
+  
+  
