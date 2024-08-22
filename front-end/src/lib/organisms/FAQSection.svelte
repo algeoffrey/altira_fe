eb4b1ef49ebefe5001cd82bf5d5a06d51aa6daa6
algeoffrey <algaza@tebengan.id>
2024-08-22 09:06:47 +0700

    Relayout FAQ section intro paragraph

--- front-end/src/lib/organisms/FAQSection.svelte
+++ front-end/src/lib/organisms/FAQSection.svelte
@@ -34,7 +34,7 @@
 <section class="bg-[#F8F8F8]">
     <section class="py-8 pt-12 mb-4 md:pt-16 md:py-12 px-4 md:px-8 lg:px-12 max-w-full md:max-w-7xl mx-auto">
         <h2 class="text-center text-2xl md:text-[2rem] font-semibold mb-6">{title}</h2>
-        <p class="text-center text-sm md:text-[1.2rem] font-thin mb-8 px-4 md:px-8 lg:px-14 mx-auto">{description}</p>
+        <p class="text-center text-sm md:text-[1.2rem] font-thin mb-8 px-4 md:px-8 lg:px-14 mx-auto leading-relaxed">{description}</p>
         
         <div class="text-base md:text-[1.15rem]">
         {#each faqs as faq}
